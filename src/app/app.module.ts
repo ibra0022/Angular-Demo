947e29afe915b8e8cea9c98e6e277d4695ddcac2
Ibrahim Alobaysi <Ibrahim.Alobaysi@tuwaiq.edu.sa>
2021-09-07 00:27:36 +0300

    adding some components part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'go to school',
+    day: 'munday',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'go to home',
+    day: 'sunday',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'go to hospital',
+    day: 'friday',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'go to desert',
+    day: 'tuesday',
+    reminder: false,
+  },
+];
